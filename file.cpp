a5f8b657aab2ebd642255a3f9becd4e7f19b8972
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2024-02-16 21:03:59 +0600

    deleting...

--- file.cpp
+++
@@ -1,6 +0,0 @@
-#include <bits/stdc++.h>
-
-int main(int argc, char const *argv[]) {
-  std::cout << "Hello World from 1" << std::endl;
-  return 0;
-}
--- folder/test2.cpp
+++
@@ -1,6 +0,0 @@
-#include <bits/stdc++.h>
-
-int main(int argc, char const *argv[]) {
-  std::cout << "Hello World from 2" << std::endl;
-  return 0;
-}
