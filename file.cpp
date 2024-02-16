c2d7c455e7c5ff8b7ae978f3dfb51a79f6fb5de0
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2024-02-16 21:00:40 +0600

    new

---
+++ file.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char const *argv[]) {
+  std::cout << "Hello World from 1" << std::endl;
+  return 0;
+}
---
+++ folder/test2.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char const *argv[]) {
+  std::cout << "Hello World from 2" << std::endl;
+  return 0;
+}
